5af21f7e767ba9818f6cca5d9af97b5e99bf1acf
jieyeo <yeojie333@gmail.com>
2024-03-22 20:53:38 -0500

    jie added process timestamp callkey and process account open date

--- preprocess.py
+++ preprocess.py
@@ -13,3 +13,20 @@ class preprocess():
     
     def process_mos_one_hot(self,df):
         pass
+
+    def process_timestamp_call_key(self, df):
+        df["timestamp"] = (df["timestamp_call_key"].str.slice(start = 0, stop = 8))
+        df["timestamp_year"] = (df["timestamp_call_key"].str.slice(start = 0, stop = 2))
+        df["timestamp_month"] = (df["timestamp_call_key"].str.slice(start = 2, stop = 4))
+        df["timestamp_day"] = (df["timestamp_call_key"].str.slice(start = 4, stop = 6))
+        df["timestamp_hour"] = (df["timestamp_call_key"].str.slice(start = 6, stop = 8))
+        df["timestamp_min"] = (df["timestamp_call_key"].str.slice(start = 8, stop = 10))
+        df["call_key"] = (df["timestamp_call_key"].str.slice(start = 11, stop = 21))
+        return df
+    
+    def process_account_open_date(self, df):
+        df['account_open_year'] = df['account_open_date_13_march'].dt.year
+        df['account_open_month'] = df['account_open_date_13_march'].dt.month
+        df['account_open_day'] = df['account_open_date_13_march'].dt.day
+        df["account_age_years"] = 2024 - df['account_open_date_13_march'].dt.year
+        return df
